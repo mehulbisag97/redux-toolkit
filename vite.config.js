cb22353d5d56918a1df943b647360c55a7ed94f5
mehul <you@example.com>
2023-08-01 16:59:58 +0530

    server changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Set to '0.0.0.0' to listen on all available network interfaces
+    port: 3000, // Set to the desired port number
+  },
+});
